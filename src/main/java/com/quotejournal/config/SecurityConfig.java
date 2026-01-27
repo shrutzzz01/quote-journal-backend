1ad7edeb0aee3190a43afc2ac71d346ec7bfd523
shrutzzz01 <shruti.tiwari2021@vitstudent.ac.in>
2026-01-27 21:47:50 +0530

    feat: edited security for deployment

--- src/main/java/com/quotejournal/config/SecurityConfig.java
+++ src/main/java/com/quotejournal/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                                 "/v3/api-docs.yaml",
                                 "/h2-console/**"  // ✅ Add this line
                         ).permitAll()
-                        .requestMatchers("/admin/**", "/dashboard/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/admin/**", "/dashboard/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers
