b51a2cf36eb0d638a691b7be93a4d6125fcd1534
shrutzzz01 <shruti.tiwari2021@vitstudent.ac.in>
2026-01-27 21:02:49 +0530

    feat: edited security for deployment

--- src/main/java/com/quotejournal/config/CorsConfig.java
+++ src/main/java/com/quotejournal/config/CorsConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import java.util.Arrays;
 
@@ -12,12 +14,12 @@ import java.util.Arrays;
 public class CorsConfig {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE) // 👈 Ensures CORS is handled before Security
     public CorsFilter corsFilter() {
-        String vercelPattern = "https://" + "*.vercel.app";
         CorsConfiguration config = new CorsConfiguration();
 
         // Allow requests from your React app
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://localhost:3002", vercelPattern));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://quote-journal-frontend-seven.vercel.app"));
 
         // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
--- src/main/java/com/quotejournal/config/SecurityConfig.java
+++ src/main/java/com/quotejournal/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable()// ✅ Disable CSRF for H2
                 )
                 .authorizeHttpRequests(auth -> auth
