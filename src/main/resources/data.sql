ea8e31f9a5d42851a8659814f7023f09b9934f77
shrutzzz01 <shruti.tiwari2021@vitstudent.ac.in>
2026-01-29 21:37:25 +0530

    feat: edited applications.properties for deployment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-insert into APP_USER(NAME, EMAIL, PASSWORD, ROLE, IS_VERIFIED) values(
-     'Shruti',
-     'shruti01@gmail.com',
-     '$2a$10$7pBOAmjgXQhmZMhpAvDM/uCaWJDtHT22kmkYHY9zH.Ck3SDlbTEYS',
-     'ADMIN',
-     true
-)
+INSERT INTO APP_USER (NAME, EMAIL, PASSWORD, ROLE, IS_VERIFIED) 
+VALUES ('Shruti', 'shruti01@gmail.com', '$2a$10$7pBOAmjgXQhmZMhpAvDM/uCaWJDtHT22kmkYHY9zH.Ck3SDlbTEYS', 'ADMIN', true);
