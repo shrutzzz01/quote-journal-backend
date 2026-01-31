e21da3c3ec75907ff673041baeb85ec3e193e5fd
shrutzzz01 <shruti.tiwari2021@vitstudent.ac.in>
2026-01-31 18:09:17 +0530

    feat: edited DATA.SQL for deployment

--- src/main/java/com/quotejournal/controller/AdminController.java
+++ src/main/java/com/quotejournal/controller/AdminController.java
@@ -6,6 +6,7 @@ import com.quotejournal.dto.UserResponse;
 import com.quotejournal.service.AdminService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +19,8 @@ public class AdminController {
     }
     @GetMapping("/dashboard")
     public ResponseEntity<AdminDashboardResponse> getDashboard(){
+        System.out.println("Current User Authorities: " +
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities());
         AdminDashboardResponse dashboardData=adminService.getDashboardData();
         return ResponseEntity.ok(dashboardData);
     }
